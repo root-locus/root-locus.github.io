e28e15e6919294b988c997aa3274ba3997250378
Denis Kutin <dekutin@yandex-team.ru>
2016-07-02 19:47:02 +0300

    fl-aur

---
+++ fl-aur.com/js/random.js
@@ -0,0 +1,43 @@
+$(function() {
+	
+	var menuItems = ["#id1", "#id2", "#id3", "#id4", "#id5", "#id6"];
+	var clikedItem = "";
+
+	$("#lock").click(function (event) {
+		$("div.sky").css({
+			'-webkit-animation-play-state': 'running', 'animation-play-state': 'running'
+		});
+		$("div.menu").css({
+			'-webkit-animation-play-state': 'running', 'animation-play-state': 'running'
+		});
+		$("div.gate").css({
+			'-webkit-animation-play-state': 'running', 'animation-play-state': 'running'
+		});
+	});
+
+	$(document).click(function (event) {
+		if ( clikedItem ) {
+			for (var i in menuItems) {
+				$(menuItems[i]).css({"opacity": "1"});
+				$(menuItems[i]+"-front").css({"cursor": "pointer"});
+				if ( clikedItem === menuItems[i] ) {
+					document.querySelector(menuItems[i]).classList.toggle("flip");
+					$(menuItems[i]).css({'transform': 'initial', 'z-index': '1' });
+				};
+			};
+			clikedItem = "";
+		} else if ( event.target.parentElement.parentElement.parentElement.id ) {
+			for (var i in menuItems) {
+				if ( menuItems[i] !== "#"+event.target.parentElement.parentElement.parentElement.id ) {
+					$(menuItems[i]).css({"opacity": "0.2"});
+					$(menuItems[i]+"-front").css({"cursor": "default"});
+				} else {
+					clikedItem = "#"+event.target.parentElement.parentElement.parentElement.id;
+					document.querySelector(menuItems[i]).classList.toggle("flip");
+					$(clikedItem).css({"z-index": "2"});
+					//$(".front").css({'-webkit-animation-play-state': 'running', 'animation-play-state': 'running'});
+				};
+			};
+		};
+	});
+});
