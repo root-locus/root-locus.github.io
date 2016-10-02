cfcb3e0f2da2d00b7a4d81df90347dd6d2d87920
Denis Kutin <dekutin@yandex-team.ru>
2016-10-02 15:12:43 +0300

    v0.3

---
+++ catalog/code.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	$("#owl-bouquets").owlCarousel({
+	/*	autoPlay: 3000, //Set AutoPlay to 3 seconds */
+		lazyLoad : true,
+		singleItem : true,
+		autoHeight : true,
+		transitionStyle:"fade"
+	});
+});
