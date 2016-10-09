ef01a48ab5e2490a548d2c0ec74e072ba69d2217
Denis Kutin <dekutin@yandex-team.ru>
2016-10-10 01:25:13 +0300

    new approach: no modal windows, refactoring

--- catalog/code.js
+++ catalog/code.js
@@ -1,23 +1,75 @@
 $(document).ready(function() {
-
 	$("#owl-bouquets").owlCarousel({
-	/*	autoPlay: 3000, //Set AutoPlay to 3 seconds */
 		lazyLoad : true,
-		singleItem : true,
-		autoHeight : true,
+		//singleItem : true,
+		//autoHeight : true,
 		transitionStyle:"fade",
-		jsonPath : 'data.json',
- 		jsonSuccess : customDataSuccess
+		jsonPath : 'bouquets.json'
 	});
-	
-	function customDataSuccess(data){
+
+	$("#owl-compositions").owlCarousel({
+		lazyLoad : true,
+		//singleItem : true,
+		//autoHeight : true,
+		transitionStyle:"fade",
+		jsonPath : 'compositions.json'
+	});
+
+	$("#owl-pots").owlCarousel({
+		lazyLoad : true,
+		//singleItem : true,
+		//autoHeight : true,
+		transitionStyle:"fade",
+		jsonPath : 'pots.json'
+	});
+
+	$("#owl-artificials").owlCarousel({
+		lazyLoad : true,
+		//singleItem : true,
+		//autoHeight : true,
+		transitionStyle:"fade",
+		jsonPath : 'artificials.json'
+	});
+
+	$("#owl-decor").owlCarousel({
+		lazyLoad : true,
+		//singleItem : true,
+		//autoHeight : true,
+		transitionStyle:"fade",
+		jsonPath : 'decor.json'
+	});
+
+	$("#owl-anniversaries").owlCarousel({
+		lazyLoad : true,
+		//singleItem : true,
+		//autoHeight : true,
+		transitionStyle:"fade",
+		jsonPath : 'anniversaries.json'
+	});
+
+	$("#owl-misc").owlCarousel({
+		lazyLoad : true,
+		//singleItem : true,
+		//autoHeight : true,
+		transitionStyle:"fade",
+		jsonPath : 'misc.json'
+	});
+
+	$("#owl-brides").owlCarousel({
+		lazyLoad : true,
+		//singleItem : true,
+		//autoHeight : true,
+		transitionStyle:"fade",
+		jsonPath : 'brides.json'
+	});
+
+
+/*	function customDataSuccess(data){
 		var content = "";
 		for(var i in data["items"]){
 			var img = data["items"][i].img;
-			var desc = data["items"][i].desc;
-			//console.log(img);
- 			content += "<img class=\"lazyOwl\" data=\""img"\" alt=\"Букет\"><div class=\""desc"\">desc</div>";
+ 			content += "<img class=\"lazyOwl\" data-src=\""+img+"\" alt=\"Букет\"/>";
 		}
 	$("#owl-bouquets").html(content);
-	}
+	}*/
 });
