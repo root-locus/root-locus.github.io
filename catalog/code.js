4356c91f9a1ece92821a86cc191bb714af85dd5a
Denis Kutin <dekutin@yandex.ru>
2017-05-14 22:13:31 +0300

    p.1: -owl-carousel

--- catalog/code.js
+++ catalog/code.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+	var url = window.location.href.split("/");
+	var curPage = url[url.length-1].split(".")[0];
+	curPageJSON = curPage + ".json";
+	curPageId = "div#owl-"+curPage;
+
+	$.getJSON(curPageJSON, function(data) {
+	    $.each(data.owl, function(i,f) {
+	        $(curPageId).append(f.item);
+	    });
+	});
+
 	$(".fancybox-thumbs").fancybox({
 		afterShow: function() {
 			$(".fancybox-title").wrapInner('<div />').show();
@@ -26,118 +37,4 @@ $(document).ready(function() {
 			}
 		}
 	});
-
-	$("#owl-bouquets").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'bouquets.json'
-	});
-
-	$("#owl-compositions").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'compositions.json'
-	});
-
-	$("#owl-pots").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'pots.json'
-	});
-
-	$("#owl-simple").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'simple.json'
-	});
-
-	$("#owl-artificials").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'artificials.json'
-	});
-
-	$("#owl-decor").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'decor.json'
-	});
-
-	$("#owl-handmade").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'handmade.json'
-	});
-
-	$("#owl-dried").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'dried.json'
-	});
-
-	$("#owl-anniversaries").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'anniversaries.json'
-	});
-
-	$("#owl-ny").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'ny.json'
-	});
-
-	$("#owl-official").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'official.json'
-	});
-
-	$("#owl-misc").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'misc.json'
-	});
-
-	$("#owl-brides").owlCarousel({
-		lazyLoad : true,
-		//singleItem : true,
-		//autoHeight : true,
-		transitionStyle:"fade",
-		jsonPath : 'brides.json'
-	});
-
-
-/*	function customDataSuccess(data){
-		var content = "";
-		for(var i in data["items"]){
-			var img = data["items"][i].img;
- 			content += "<img class=\"lazyOwl\" data-src=\""+img+"\" alt=\"Букет\"/>";
-		}
-	$("#owl-bouquets").html(content);
-	}*/
 });
